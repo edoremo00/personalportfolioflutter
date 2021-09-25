2900d7fe94e6a7da6453ba63399d62cf6afa04de
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-26 01:45:16 +0200

    inizializzata pagina projects mobile

---
+++ lib/widgets/projectsmobilepage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Projectsmobilepage extends StatelessWidget {
+  const Projectsmobilepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
