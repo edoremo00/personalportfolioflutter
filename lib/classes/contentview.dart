eb2ebcac8d16e6f73ba7606d362b0b59e96420e1
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-09-26 01:44:56 +0200

    import change

--- lib/classes/contentview.dart
+++ lib/classes/contentview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:testportfolio/classes/customtab.dart';
+import 'package:testportfolio/widgets/customtab.dart';
 
 class Contentview {
   Contentview({required this.tab, required this.content, this.icona});
